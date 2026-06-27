eeabb1ff7522d39cd313b6f79184c8f9149e04d0
troymcgahey <tmcgahey93@gmail.com>
2026-06-26 21:43:03 -0400

    Code main.py

--- src/rag_mcp_agent/main.py
+++ src/rag_mcp_agent/main.py
@@ -1,5 +1,25 @@
-def main():
-    print("Hello from langgrpah-rag-mcp!")
+from rag_mcp_agent.graph import build_graph
+
+def main() -> None:
+    graph = build_graph()
+
+
+    result = graph.invoke(
+        {
+            "question": "What city is good for visiting Pompeii?",
+            "documents": [],
+        }
+    )
+
+    print("\nQuestion:")
+    print(result["question"])
+
+    print("\nRetrieved Documents:")
+    for index, document in encumerate(result["documents"], start=1):
+        print(f"\nResult {index}"}
+        print("Source:", document.metadata.get("source"))
+        print("Content:", document.page_content)
+        
 
 
 if __name__ == "__main__":
